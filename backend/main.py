3225c9718cd217e2a61d0fe9406fbc669532a96f
David Lee <leetdavidu@gmail.com>
2022-03-31 01:41:43 +0800

    added font

--- backend/main.py
+++ backend/main.py
@@ -16,6 +16,10 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+smallFont = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSansBold.ttf', 20)
+largeFont = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSansBold.ttf', 27)
+
+
 def draw_image(image, epd=epd):
     epd.prepare()
     epd.display(image)
@@ -23,3 +27,10 @@ def draw_image(image, epd=epd):
 @app.get('/text')
 async def render_text(author, text):
     image = Image.new('1', (epd.width, epd.height), 255)
+    draw = ImageDraw.Draw(image)
+
+    # Top "Author Said" Box
+    draw.rectangle((0, 0, epd.width, int(0.227 * epd.height)), fill=0)
+    draw.text((10, 10), author, font=smallFont, fill=255)
+
+    
